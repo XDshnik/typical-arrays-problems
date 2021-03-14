ce08e2b4acd86720d466d7b6f11836e37c16b633
XDshnik <pesherovpavel@gmail.com>
2021-03-14 12:50:16 +0300

    feat: add functionality for min,max,avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-
+// I wouldn't use any sort here before searching
+  // for the minimum value in array because it will become
+  // easier to solve the task :) but i know i should do this
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    if (array.length === 1) {
+      return array;
+    } else {
+      let min = array[0];
+      for (let i = 0; i<array.length; i++) {
+        (array[i]<min) ? (min=array[i]) : i=i;
+      }
+      return min
+    }
+  }  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }  else {
+    if (array.length === 1) {
+      return array;
+    } else {
+      let max = array[0];
+      for (let i = 0; i<array.length; i++) {
+        (array[i]>max) ? (max=array[i]) : i=i;
+      }
+      return max
+    }
+  }  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let avg = 0;
+    for (let i = 0 ; i<array.length; i++) {
+      avg += array[i]
+    }
+  return (avg/array.length);
+  }
 }
